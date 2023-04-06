09530fc6b6380f39da30f6a865b591b955019168
Vernli <dawid_krajewski@outlook.com>
2023-04-06 22:45:26 +0200

    search form

--- dist/script.js
+++ dist/script.js
@@ -1,18 +1,22 @@
+const searchBtn = document.querySelector("#search-btn");
+
 window.addEventListener("scroll", () => {
   const navBar = document.querySelector("#menu");
   navBar.classList.toggle("bg-black", window.scrollY > 0);
   navBar.classList.toggle("opacity-90", window.scrollY > 0);
 });
 
-const searchBtn = document.querySelector("#search-btn");
-
 searchBtn.addEventListener(
   "click",
   () => {
     const input = document.createElement("input");
+    input.setAttribute("name", "search");
+    input.setAttribute("id", "search");
+    input.setAttribute("type", "text");
     input.setAttribute("placeholder", "Search");
     input.classList.add("search-input");
-    document.querySelector("#left-menu-elements").appendChild(input);
+
+    document.querySelector("#search-form").appendChild(input);
   },
   { once: true }
 );
